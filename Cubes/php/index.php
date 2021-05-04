72ee06827316e4a40ac1d854fc7d7918f829532d
Александр <57570859+BeaverHorror@users.noreply.github.com>
2021-05-04 18:29:56 +0300

    Add files via upload

---
+++ Cubes/php/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Заголовок</title>
+  </head>
+  
+  <body> 
+    
+    <?php 
+      // ../other/img/
+	  
+	  $txt = "";
+      if(isset($_GET['txt'])) $txt = $_GET['txt'];
+	  //echo $txt;
+
+      $dir_path = "../".$txt;
+      $scan = scandir($dir_path);
+      //print_r($scan);
+    
+      for($i = 2; $i < count($scan); $i++){
+        $filename = $scan[$i];
+        $format = substr($filename,strpos($filename,"."));
+        if($format == ".jpg" || $format == ".png"){
+          //echo "<br />".$scan[$i]; 
+          echo "<img width='100' src=".$dir_path."/".$scan[$i]." />";
+          echo $scan[$i]; 
+          //echo $format;
+        }
+      }
+ 
+      //echo $txt;
+
+    ?>
+    
+  </body>
+
+</html>
